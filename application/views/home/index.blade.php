03260c2e515334f80fdb3c10851c5bceedded65e
stefanledin <stefan.ledin@gmail.com>
2012-11-13 06:23:01 +0100

    Created the form

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
 	<h1>Helloooooo</h1>
+	{{ Form::open('/') }}
+		{{ Form::label('name', 'Sorry to disapoint you')}}
+		{{ Form::text('name') }}
+		{{ Form::label('device', 'but your')}}
+		{{ Form::text('device') }}
+		{{ Form::label('disarmed', 'has been')}}
+		{{ Form::text('disarmed') }}
+		{{ Form::submit('Skicka')}}
+	{{ Form::close() }}
 @endsection
